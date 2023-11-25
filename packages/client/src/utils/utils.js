2d14fe5bed86dd2219c046b3fc11e886ffce6d3b
Harsh <harsh94664@gmail.com>
2023-11-25 18:51:37 +0530

    fix issue

--- packages/client/src/utils/utils.js
+++ packages/client/src/utils/utils.js
@@ -1,4 +1,5 @@
 export const serverURL = "https://chatzone-1awf.onrender.com";
+// export const serverURL = "http://127.0.0.1:4000";
 
 export const hasKeys = (obj = {}) => {
   return obj && Object.keys(obj).length > 0 ? true : false;
